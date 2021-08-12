f39ff61cb3cafc3eadfa2bb5e407a4ce11daffdd
gamzesakallioglu <gamzesakallioglu0@gmail.com>
2021-08-12 17:51:57 +0300

    AreasAdded

---
+++ VetTrack.DataAccess/Data/Migrations/20210811232115_AddVetTrackToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VetTrack.Data.Migrations
+{
+    public partial class AddVetTrackToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ VetTrack.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetTrack.Utility
+{
+    public class Class1
+    {
+    }
+}
